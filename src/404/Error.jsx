1dbf8ce0104357e762658818641c9bc1f07364c7
Emon <emon50666@gmail.com>
2024-04-27 13:00:36 +0600

    add 404 page

--- src/404/Error.jsx
+++ src/404/Error.jsx
@@ -1,12 +1,16 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import errorImg from '../assets/404.png'
+import { Link } from "react-router-dom";
 
 const Error = () => {
 
     return (
-        <div className="text-center">
+        <div className="text-center ">
             <Header></Header>
-            <h1 className="font-bold text-2xl mt-10">Opps Page Not Found !</h1>
+            <img src={errorImg} alt="" className="w-[300px] justify-center mx-auto"/>
+            <h1 className="font-bold text-2xl mt-4 mb-4">Opps Page Not Found !</h1>
+           <Link to={'/'}> <button className="bg-[#E86822]  buton md:mr-4 text-white rounded-lg pt-2 pb-2 pr-4 pl-4 mt-2 mb-10 font-bold ">Back to Home</button> </Link>
             <Footer></Footer>
         </div>
     );
--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -24,14 +24,14 @@ const Home = () => {
         <div id="slide2" className="carousel-item relative w-full">
         <img src={art_2} className="w-full md:h-[450px] " />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn  btn-circle">❮</a> 
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
         </div> 
         <div id="slide3" className="carousel-item relative w-full">
         <img src={art_3} className="w-full md:h-[450px] " />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn  btn-circle">❮</a> 
             <a href="#slide1" className="btn btn-circle">❯</a>
           </div>
         </div> 
