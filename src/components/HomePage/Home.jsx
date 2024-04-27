0e31da2239d758084192139099f0586d9bc01022
Emon <emon50666@gmail.com>
2024-04-27 12:50:24 +0600

    add home page banner

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,10 +1,42 @@
 
+import art_1 from '../../assets/art-1.png'
+import art_2 from '../../assets/art-2.png'
+import art_3 from '../../assets/art-3.png'
+
+
+
 const Home = () => {
     return (
-        <div>
-            <h1>this is home page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A eligendi ea, molestiae dolorem aliquid libero quibusdam ducimus cum reiciendis beatae minus optio accusamus, ipsa corporis facere enim autem, magni porro.</p>
-        </div>
+       
+        <div className="carousel w-full ">
+        <div id="slide1" className="carousel-item relative w-full">
+          
+          <img src={art_1} className="w-full md:h-[450px] "  />
+          
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5  top-1/2 ">
+             
+            <a href="#slide4" className="btn btn-circle ">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
+           
+          </div>
+         
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+        <img src={art_2} className="w-full md:h-[450px] " />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+        <img src={art_3} className="w-full md:h-[450px] " />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+       
+                  </div>
     );
 };
 
