a1429d084a1733b1db0f6938558f4c50344858a6
Emon <emon50666@gmail.com>
2024-04-29 00:41:05 +0600

    add view details page and show all  content

--- src/components/ViewCarftDetails/ViewCarftDetails.jsx
+++ src/components/ViewCarftDetails/ViewCarftDetails.jsx
@@ -12,7 +12,7 @@ const ViewCarftDetails = () => {
 
    
   
-    const { item, description, image,price,rating,processing } = carfts;
+    const { item, description, image,price,rating,processing ,customization,stockStatus,subcategory,_id,email,username} = carfts;
 
 
    
@@ -23,17 +23,27 @@ const ViewCarftDetails = () => {
     return (
         <div className="container mx-auto mt-10">
            <div className="card bg-base-100 shadow-xl shadow-yellow-300/25 mb-4">
-                <figure><img src={image} alt="" className="w-full h-[400px] " /></figure>
+                <figure><img src={image} alt="" className="w-full md:h-[400px] " /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{item}</h2> 
+                   <div className="md:flex justify-between">
+                   <h2 className="card-title  text-3xl">{item}</h2> 
+                   <h2 className=" text-base text-orange-600 ">Id: {_id}</h2> 
+
+                   </div>
                     <p className="text-gray-500">{description}</p> 
-                    <div className="flex">
+                    <div className="md:flex text-center">
                         <p className="flex gap-1 items-center"> <GoDotFill></GoDotFill> {price.slice(0,6)} </p>
                         <p className="flex gap-1 items-center"> <GoDotFill></GoDotFill> {rating.slice(0,6)} </p>
                         <p className="flex gap-1 items-center"> <GoDotFill></GoDotFill> {processing} </p>
+                        <p className="flex gap-1 items-center"> <GoDotFill></GoDotFill> {customization} </p>
+                        <p className="flex gap-1 items-center"> <GoDotFill></GoDotFill> {stockStatus} </p>
+                        <p className="flex gap-1 items-center"> <GoDotFill></GoDotFill> {subcategory} </p>
+                    </div>
+                    <div className="border-t border-dashed">
+                        <li> {email} </li>
+                        <li> {username} </li>
 
                     </div>
-                
                 </div>
             </div>
         </div>
